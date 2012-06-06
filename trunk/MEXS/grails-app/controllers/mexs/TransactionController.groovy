686639b2dac194f5f60fdbb88c82d4e7fe4b0b05
bp5837@gmail.com <bp5837@gmail.com@4b4175e1-9e47-18be-ecfa-9c8c5caa460c>
2012-06-06 06:53:04 +0000

    final updates

--- trunk/MEXS/grails-app/controllers/mexs/TransactionController.groovy
+++ trunk/MEXS/grails-app/controllers/mexs/TransactionController.groovy
@@ -1,9 +1,70 @@
 package mexs
 
+import java.nio.channels.Pipe.SourceChannel;
+
+import org.granite.tide.annotations.TideEnabled
+
+@TideEnabled
 class TransactionController {
 
+	double totalCommission
+	double totalAmount
+	double totalMinusCommission
+	
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	
+	def calcTotalAmount = {
+		def srcAmt = params.srcAmt.replace(",", "").replace("\$","").toDouble()
+		def srcCode = params.srcCode
+		def tgtCode = params.tgtCode
+		
+		def sourceCurrency = Currency.findByCurrencySymbol(params.srcCode)
+		def targetCurrency = Currency.findByCurrencySymbol(params.tgtCode)
+		
+		totalAmount = (srcAmt * sourceCurrency.sellingRate) * targetCurrency.buyingRate;
+	}
 
+	def calcCommission = {
+		totalAmount = params.totalAmount
+		if(params.totalAmount < 100){
+			totalCommission = 3.00
+		}
+		if(params.totalAmount > 100 && params.totalAmount <= 500){
+			totalCommission = 2.50
+		}
+		if(params.totalAmount > 500 && params.totalAmount <= 1000){
+			totalCommission = 2.00
+		}
+		if(params.totalAmount > 1000){
+			totalCommission = 1.50
+		}
+	}
+	
+	def calcTotalMinusCommission = {
+		def targetCurrency = Currency.findByCurrencySymbol(params.targetCode)
+		def commissionByTarget = targetCurrency.buyingRate * params.totalCommission
+		
+		totalMinusCommission = params.totalAmount - commissionByTarget
+	}
+	
+	def createTransaction = {
+		def transactions = []
+		def patron = params.patron
+		def source = Currency.findByCurrencySymbol(params.sourceCurrency.currencySymbol)
+		def target = Currency.findByCurrencySymbol(params.targetCurrency.currencySymbol)
+		def transaction = params.transaction
+		
+		transaction.terminalID = Terminal.findByTerminalID(1)
+		transaction.patronID = patron
+		transaction.employeeID = Employee.findByEmployeeID(1)
+		transaction.sourceCurrencyID = source
+		transaction.targetCurrencyID = target
+		
+		transactions.add(transaction)
+		
+		[transactions : transactions]
+	}
+	
     def index = {
         redirect(action: "list", params: params)
     }
